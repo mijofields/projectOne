46ecd03ac4ff6227fe7814e74e6cd9b93026c6fe
dev2433 <devsunilpatel@gmail.com>
2018-01-08 13:23:15 -0600

    added-charts-fixed-messages

--- crypLIFE/stream.js
+++ crypLIFE/stream.js
@@ -98,3 +98,32 @@ $(document).ready(function() {
                 $('#messages').append(($('<h></h>').text(data.message)));
                 $('#messages').append(($('<p>').text("")));
             });
+            // general config for all charts
+var config = {
+    presetColorScheme: 'albuquerque',
+    timePeriod: '30m'
+}
+
+// exchange, currency pair, html element id
+var charts = [
+    ['bitfinex', 'btcusd', 'one'],
+    ['kraken', 'btceur', 'two'],
+    ['okcoin', 'btccny', 'three'],
+    ['okcoin', 'btcusd-quarterly-futures', 'four']
+]
+
+// create all charts
+charts.forEach(function (chart) {
+    chart.push(new cryptowatch.Embed(chart[0], chart[1], config))
+    chart[3].mount('#' + chart[2])
+})
+
+// listen for changes in "time period" select box
+document.getElementById('timePeriod').onchange = function(event) {
+    charts.forEach(function (chart) {
+        var cell = document.getElementById(chart[2])
+        cell.removeChild(cell.firstChild)
+        chart[3].opts.timePeriod = event.target.value
+        chart[3].mount('#' + chart[2])
+    })
+}
